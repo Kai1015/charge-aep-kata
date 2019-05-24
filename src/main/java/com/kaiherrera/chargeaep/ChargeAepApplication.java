73c86d764949009485487739804dd188681655b5
Kai Herrera <kherrera@manifestcorp.com>
2019-05-24 13:17:58 -0400

    Initial commit

---
+++ src/main/java/com/kaiherrera/chargeaep/ChargeAepApplication.java
@@ -0,0 +1,13 @@
+package com.kaiherrera.chargeaep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeAepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargeAepApplication.class, args);
+	}
+
+}
